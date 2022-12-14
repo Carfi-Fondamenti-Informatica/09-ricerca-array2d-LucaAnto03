5df4720ce18a7b76e67f050ae6e05e211e844cad
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-14 17:30:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main () {
+char a [10][20] = {};
+char nome [1][20] = {};
+nome [0][19]='\0';
+int count=0;
+
+
+for (int i=0; i<10; i++){
+    a [i][19] = '\0';
+}
+
+cout << "Inserire 10 nomi: " << endl;
+
+for (int i=0; i<10; i++) {
+    cin >> a[i];
+    }
+
+cout << "Inserire nome da cercare: "<< endl;
+cin >> nome [0] ;
+
+if ( ricerca (a, nome,  count) ) {
+    cout << count << endl;
+} else {
+    cout << "non presente" << endl;
+}
+
+return 0;
 }
