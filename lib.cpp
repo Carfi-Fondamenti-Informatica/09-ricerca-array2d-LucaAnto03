764f94bfe6121a0951391301a05847ad82022565
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-14 17:33:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+
+bool ricerca ( char a [10][20], char nome [1][20] , int &count) {
+
+    for (int i=0; i<10; i++){
+        if (strcmp(a[i], nome [0]) == 0) {
+                count = i;
+                return true;
+            }
+    } return false;
+
+}
