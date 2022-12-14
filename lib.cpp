a284c396e95cc503c8869bb6ea0066dbfaa96011
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-14 17:41:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+void listanomi (char a [10][20]){
+    for (int i=0; i<10; i++) {
+        cin >> a[i];
+    }
+}
+
 bool ricerca ( char a [10][20], char nome [1][20] , int &count) {
 
     for (int i=0; i<10; i++){
